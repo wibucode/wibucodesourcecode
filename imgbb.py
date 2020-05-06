cd670c66972510f164f89d6e008a223f710d22d4
wibucode <64858530+wibucode@users.noreply.github.com>
2020-05-07 01:48:02 +0700

    Update and rename jj.py to imgbb.py

--- imgbb.py
+++ imgbb.py
@@ -1,12 +1,12 @@
 import json
-import os, sys, shutil
+import os, sys
 import base64
 import requests
 nama_file = raw_input("Nama File: ")
 with open(nama_file, "rb") as file:
 	url = "https://api.imgbb.com/1/upload"
     	payload = {
-       		 "key": "58d46e23a3edecf448226741dc15224c",
+       		 "key": "Key api imgbb",
        		 "image": base64.b64encode(file.read()),
    	 }
     	res = requests.post(url, payload)
